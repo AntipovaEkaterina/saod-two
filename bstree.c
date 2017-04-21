61653aef3639714b493f1f0eaf44e59842d5a92a
Антипова Екатерина Петровна <iv621s01@lwp02.csc.local>
2017-04-21 12:31:14 +0700

    Bstree

--- bstree.c
+++ bstree.c
@@ -1,15 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
 struct bstree *bstree_create(char *key, int value){
-
-}
-void bstree_add(struct bstree *tree, char *key, int value){
-
-}
-struct bstree *bstree_lookup(struct bstree *tree, char *key){
-
-}
+	bstree_create(words[0], 0); 
+	for (i = 2; i <= 200000; i++){
+		void bstree_add(struct bstree *tree, char *key, int value){
+		if i % 10000 == 0
+		}
+	}
+	w = word[getRand(0, i —1)];
+	t == wtime();
+	struct bstree *bstree_lookup(struct bstree *tree, char *key){
+		t = wtime() - t;
+		print(«n = %d; time = %.6f», i — 1, t);
+	}
 struct bstree *bstree_min(struct bstree *tree){
 
 }
 struct bstree *bstree_max(struct bstree *tree){
 
+}
+return 0;
 }
